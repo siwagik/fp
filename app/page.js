769102cc77435e5046e7e75c1fa6b082167c6294
siwagik <siwagejob@gmail.com>
2023-06-03 02:10:41 +0300

    zbt

--- app/page.js
+++ app/page.js
@@ -1,95 +1,95 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Link from "next/link";
+import './page.css'
+import { Jost } from 'next/font/google'
+
+const jost = Jost({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className='app'>
+      <div className="container">
+        <div className="header">
+          <header>
+            <h1 className="logo">FantasticPower</h1>
+            <nav>
+              <Link href="/" className="link ">Главная</Link>
+              <Link href="/rules" className="link ">Правила</Link>
+              <Link href="/resource-pack" className="link ">Ресурс-пак</Link>
+              <Link href="https://fantasticpower.ru/" className="link ">Форум</Link>
+            </nav>
+          </header>
+        </div>
+        <div className="main">
+            <div className="left">
+              <div className="left_information">
+                <h1>Сеть увлекательных<br/>игровых серверов</h1>
+                <p className="friend">Новые приключения ждут тебя и твоих друзей.<br/>Окунись в новый мир Minecraft вместе с <span className="fp">FantasticPower</span></p>
+                <div className="buttonOnline">
+                  <div className="button">
+                      <p>Начать игру</p>
+                      <img src="/play.png"/>
+                  </div>
+                  <div className="online">
+                    <div className="circle"></div>
+                    <h2>Сейчас в игре: <span>1200</span></h2>
+                  </div>
+                </div>
+              </div>
+              <div className="scroll">
+                <img src="/arrow.png"/>
+                <p>Прокрутите вниз</p>
+              </div>
+            </div>
+          <div className="right">
+            <img src="/character.png"/>
+          </div>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="how_to_start">
+        <div className="hts_container">
+          <h1 className="hts_h1">Как начать игру?</h1>
+          <div className="cards">
+            <div className="card1 card">
+              <div className="card_content">
+                <h1 className="cardh1">Шаг 1</h1>
+                <p className="cardp">Для начала игры на нашем проекте, вам нужно скачать игру Minecraft, вы можете сделать это бесплатно на сайте TLaucnher</p> 
+              </div>
+            </div>
+            <div className="card2 card">
+              <div className="card_content">
+                <h1 className="cardh1">Шаг 2</h1>
+                <p className="cardp">Что-бы играть на нашем проекте, вам нужно скачать любую версию в диапозоне: 1.16 - 1.19</p>
+              </div>
+            </div>
+            <div className="card3 card">
+              <div className="card_content">
+                <h1 className="cardh1">Шаг 3</h1>
+                <p className="cardp">Нужно добавить наш IP в список серверов, и сможете приступить к игре :3</p>
+                <div className="cardButton">
+                  <div className="Button">
+                    <p className="cardButtonP">fantasticpower.ru</p>
+                    <img src="/copy.png"/>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="separator"></div>
+        <footer>
+        <div className="fantasticpower">
+          <p>FantasticPower не связан с Mojang. Все средства идут на<br/>развитие проекта</p>
+        </div>
+        <div className="social">
+          <a href="https://vk.com/fantasticpowerik" className="vk" target="_blank">
+            <img src="/vk.png"/>
+          </a>
+          <a href="https://discord.com/invite/8Tu6Wzyqbu" className="discord" target="_blank">
+            <img src="/discord.png"/>
+          </a>
+        </div>
+      </footer>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
