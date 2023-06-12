2b40bb312fc78175e44069be0656e4bb0f38dd45
siwagik <siwagejob@gmail.com>
2023-06-12 21:17:17 +0300

    release

--- app/layout.js
+++ app/layout.js
@@ -12,10 +12,6 @@ export default function RootLayout({ children }) {
         <meta property="og:title" content="FantasticPower" />
         <meta property="og:description" content="Сеть увлекательных игровых сервер в Minecraft" />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://fantasticpower.vercel.app/" />
-        <meta property="og:image" content="https://fantasticpower.vercel.app/cringe.jpg" />
-        <meta property='og:image:width' content='1200'/>
-        <meta property='og:image:height' content='1200'/>
       </head>
       <body>{children}</body>
     </html>
