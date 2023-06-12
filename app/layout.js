ab7c349db8922f6142c26f86d6949915afeb55ac
siwagik <siwagejob@gmail.com>
2023-06-12 20:18:15 +0300

    release

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,16 @@
 import { Inter } from 'next/font/google'
 
-import Head from "next/head"
 const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'FantasticPower',
+  description: 'МААААААААААААЙНКРАФТ МОЯ ЖИЗНЬ',
+  
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <title>FantascticPower</title>
-
-        <meta property='og:title' content='FantasticPower'/>
-        <meta property='og:type' content='website' />
-        <meta property='og:url' content='https://fantasticpower.vercel.app/'/>
-        <meta property='og:description' content='Описание страницы'/>
-        <meta property='og:image' content='https://fantasticpower.vercel.app/cringe.jpg'/>
-
-      </Head>
       <body>{children}</body>
     </html>
   )
