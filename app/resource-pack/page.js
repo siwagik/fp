448b64ff6680fd1b76c6a80f1348898803423825
siwagik <siwagejob@gmail.com>
2023-07-10 00:28:14 +0300

    release

--- app/resource-pack/page.js
+++ app/resource-pack/page.js
@@ -1,17 +1,32 @@
 'use client'
-
 import { useEffect } from 'react';
 
 export default function Download() {
   useEffect(() => {
-    const ready = () => {
-      var link = document.createElement('a');
-      link.setAttribute('href', 'resource-pack');
-      link.setAttribute('download', 'pack.zip');
-      link.click();
+    const downloadFile = async () => {
+      try {
+        const response = await fetch('/pack.zip');
+        const blob = await response.blob();
+
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = 'pack.zip';
+        link.click();
+
+        URL.revokeObjectURL(url);
+      } catch (error) {
+        console.log('Ошибка при скачивании файла:', error);
+      }
     };
-    ready()
+
+    downloadFile();
   }, []);
 
   return <div></div>;
 }
+
+
+
+
+
